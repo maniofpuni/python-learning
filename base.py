0efe78a7aec5c1aa4e0639354520710b5a889514
maniofpuni <bhuvimaha1994@gmail.com>
2021-04-05 13:07:48 +0530

    added new code

---
+++ base.py
@@ -0,0 +1,37 @@
+# # print ("    prihrh /")
+# # print(1+2)
+# # print(567+670)
+# # print(467+987)
+# # print(789+567)
+# # print(6789+2345)
+# # print(23578+98766)	
+
+# # def add(a,b):
+# # 	return a+b
+# # print()
+
+# print("/_____|")
+# print("     /|")
+# print("    / |")
+# print("   /  |")
+
+
+print("_______|")
+print("|      |")
+print("|_____ |")
+print("      ||")
+print("______||")
+
+
+# print("There once was a man named George,")
+# print("he was 70 years old.")
+# print("He really liked the name George")
+# print("but didn't like being 70.")
+
+
+
+
+
+print("he was 70 years old.")
+print("He really liked the name George,")
+print("but didn't like being 70.")
